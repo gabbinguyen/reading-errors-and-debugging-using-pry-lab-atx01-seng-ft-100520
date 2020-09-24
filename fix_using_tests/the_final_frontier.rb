f6834df05e9237b7a7c096d290dcfe21a9f75deb
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-09-24 21:25:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,6 +2,7 @@
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  star_date = generate_star_date
 end
 
 def state_log(star_date)
@@ -10,5 +11,4 @@ end
 
 def engage
   puts state_log(star_date)
-  star_date = generate_star_date
 end
